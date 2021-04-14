82bb8726ae77b706ed0278b0b3619d8ec75a6761
josemoncada87 <josemoncada87@hotmail.com>
2021-04-14 08:29:20 -0500

    Commit changes

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
+// Literal
+// JSON
+let obj = {
+  nombre:"Jose Andrés",
+  apellido:"Moncada Quintero",
+};
+
 function setup() {
   createCanvas(400, 400);
+  console.log(obj.apellido);
 }
 
 function draw() {
